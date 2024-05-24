4a3c277bc7ca2aba052083e29d3907bc4f198c9b
hyhy-j <lhjlhj10088@gmail.com>
2024-05-25 04:05:57 +0900

    Add files via upload

---
+++ community.java
@@ -0,0 +1,14 @@
+package exam;
+
+import javax.swing.JFrame;
+
+public class community extends JFrame{
+	public community() {
+		setTitle("community");
+		setBounds(200, 200, 300, 250);
+        setResizable(false);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+        setSize(1280, 832);
+	}
+}
---
+++ mypage.java
@@ -0,0 +1,14 @@
+package exam;
+
+import javax.swing.JFrame;
+
+public class mypage extends JFrame{
+	public mypage() {
+		setTitle("mypage");
+		setBounds(200, 200, 300, 250);
+        setResizable(false);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+        setSize(1280, 832);
+	}
+}
