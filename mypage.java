9cbe2e19e1e597d32918a08e1aca266174f18651
hyhy-j <lhjlhj10088@gmail.com>
2024-05-26 22:12:32 +0900

    Update mypage.java

--- mypage.java
+++ mypage.java
@@ -1,9 +1,15 @@
 // 5/25 이주원 (상단바 메인페이지처럼 추가)
+// 5/26 이현정 (상단바 수정)
 
 package exam;
 
+import java.awt.Color;
 import java.awt.event.ActionEvent;
+
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
 import javax.swing.*;
 
 public class mypage extends JFrame {
@@ -13,6 +19,7 @@ public class mypage extends JFrame {
         setResizable(false);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(1280, 832);
+        setBackground(Color.WHITE);
 
         createMenu(); // 상단바 함수 실행
 
@@ -24,25 +31,37 @@ public class mypage extends JFrame {
 
         JLabel leftLabel = new JLabel("myJeju");
         leftLabel.setBorder(BorderFactory.createEmptyBorder(0, 10, 0, 0));
+        leftLabel.setForeground(new Color(255, 255, 255));
         menuBar.add(leftLabel);
 
-        JMenuItem mainPageItem = new JMenuItem("mainpage");
+        JMenuItem myPageItem = new JMenuItem("mypage");
         JMenuItem communityItem = new JMenuItem("community");
+        myPageItem.setBackground(new Color(247, 125, 12));
+        myPageItem.setForeground(new Color(255, 255, 255));
+        communityItem.setBackground(new Color(247, 125, 12));
+        communityItem.setForeground(new Color(255, 255, 255));
 
-        mainPageItem.addActionListener(new mainPageActionListener());
+        leftLabel.addMouseListener(new MouseAdapter() {
+        	@Override
+        	public void mouseClicked(MouseEvent e) {
+        		new MainScreen();
+        	}
+        });
+        myPageItem.addActionListener(new myPageActionListener());
         communityItem.addActionListener(new communityActionListener());
 
         menuBar.add(Box.createHorizontalGlue());
 
-        menuBar.add(mainPageItem);
+        menuBar.add(myPageItem);
         menuBar.add(communityItem);
+        menuBar.setBackground(new Color(247, 125, 12));
 
         setJMenuBar(menuBar);
     }
 
-    private class mainPageActionListener implements ActionListener {
+    private class myPageActionListener implements ActionListener {
         public void actionPerformed(ActionEvent e) {
-            new MainScreen();
+            new mypage();
             dispose();
         }
     }
@@ -54,4 +73,3 @@ public class mypage extends JFrame {
         }
     }
 }
-
