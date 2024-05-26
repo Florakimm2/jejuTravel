a660f3eb40bb23f1d2095e42742114c4731dbee4
hyhy-j <lhjlhj10088@gmail.com>
2024-05-26 22:11:38 +0900

    Update community.java

--- community.java
+++ community.java
@@ -1,9 +1,14 @@
 // 5/25 이주원 (상단바 메인페이지처럼 추가)
+// 5/26 이현정 (상단바 수정)
 
 package exam;
 
+import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
 import javax.swing.*;
 
 public class community extends JFrame {
@@ -13,6 +18,7 @@ public class community extends JFrame {
         setResizable(false);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(1280, 832);
+        setBackground(Color.WHITE);
 
         createMenu(); // 상단바 함수 실행
 
@@ -24,34 +30,45 @@ public class community extends JFrame {
 
         JLabel leftLabel = new JLabel("myJeju");
         leftLabel.setBorder(BorderFactory.createEmptyBorder(0, 10, 0, 0));
+        leftLabel.setForeground(new Color(255, 255, 255));
         menuBar.add(leftLabel);
 
-        JMenuItem mainPageItem = new JMenuItem("mainpage");
-        JMenuItem mypageItem = new JMenuItem("mypage");
-
-        mainPageItem.addActionListener(new mainPageActionListener());
-        mypageItem.addActionListener(new mypageActionListener());
+        JMenuItem myPageItem = new JMenuItem("mypage");
+        JMenuItem communityItem = new JMenuItem("community");
+        myPageItem.setBackground(new Color(247, 125, 12));
+        myPageItem.setForeground(new Color(255, 255, 255));
+        communityItem.setBackground(new Color(247, 125, 12));
+        communityItem.setForeground(new Color(255, 255, 255));
+        
+        leftLabel.addMouseListener(new MouseAdapter() {
+        	@Override
+        	public void mouseClicked(MouseEvent e) {
+        		new MainScreen();
+        	}
+        });
+        myPageItem.addActionListener(new myPageActionListener());
+        communityItem.addActionListener(new communityActionListener());
 
         menuBar.add(Box.createHorizontalGlue());
 
-        menuBar.add(mainPageItem);
-        menuBar.add(mypageItem);
+        menuBar.add(myPageItem);
+        menuBar.add(communityItem);
+        menuBar.setBackground(new Color(247, 125, 12));
 
         setJMenuBar(menuBar);
     }
 
-    private class mainPageActionListener implements ActionListener {
-        public void actionPerformed(ActionEvent e) {
-            new MainScreen();
-            dispose();
-        }
-    }
-
-    private class mypageActionListener implements ActionListener {
+    private class myPageActionListener implements ActionListener {
         public void actionPerformed(ActionEvent e) {
             new mypage();
             dispose();
         }
     }
-}
 
+    private class communityActionListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            new community();
+            dispose();
+        }
+    }
+}
